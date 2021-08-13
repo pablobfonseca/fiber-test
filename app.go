262450ed68a4ea02e85bcda0fc403cb7d5a6c6b2
Pablo Fonseca <pablofonseca777@gmail.com>
2021-08-13 19:57:15 -0300

    basic routing

--- app.go
+++ app.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello World 👏!")
+	// Get /api/register
+	app.Get("/api/*", func(c *fiber.Ctx) error {
+		msg := fmt.Sprintf("🖐 %s", c.Params("*"))
+		return c.SendString(msg)
 	})
 
-	app.Listen(":3000")
+	// GET /flights/LAX-SFO
+	app.Get("/flights/:from-:to", func(c *fiber.Ctx) error {
+		msg := fmt.Sprintf("💸 From: %s, To: %s", c.Params("from"), c.Params("to"))
+		return c.SendString(msg)
+	})
+
+	// GET /dictionary.txt
+	app.Get("/:file.:ext", func(c *fiber.Ctx) error {
+		msg := fmt.Sprintf("📃 %s.%s", c.Params("file"), c.Params("ext"))
+		return c.SendString(msg)
+	})
+
+	// GET /john/75
+	app.Get("/:name/:age/:gender?", func(c *fiber.Ctx) error {
+		msg := fmt.Sprintf("👴 %s is %s years old", c.Params("name"), c.Params("age"))
+		return c.SendString(msg)
+	})
+
+	// GET /john
+	app.Get("/:name", func(c *fiber.Ctx) error {
+		msg := fmt.Sprintf("Hello %s 👋!", c.Params("name"))
+		return c.SendString(msg)
+	})
+
+	log.Fatal(app.Listen(":3000"))
 
 }
